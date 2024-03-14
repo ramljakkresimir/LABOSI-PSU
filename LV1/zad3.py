2ef723c4c9dd2036e55e82a4601bef9376c4f351
ramljakkresimir <95712829+ramljakkresimir@users.noreply.github.com>
2024-03-14 19:00:39 +0100

    Update zad3.py

--- LV1/zad3.py
+++ LV1/zad3.py
@@ -1,1 +1,21 @@
+numbers = []
+while(True):
+    print("Unesi broj ili Done za kraj:" )
+    unos = input()
+    if unos.lower() == 'done':
+        break
+    try:
+        number = float(unos)
+        numbers.append(number)
+    except ValueError:
+        print("Molimo unesite broj ili Done za kraj")
 
+if len(numbers) == 0:
+    print("Niste unijeli niti jedan broj ")    
+else:
+    print("Brojeva uneseno: " ,len(numbers))
+    print("Srednja vrijednost: ",sum(numbers) / len(numbers))
+    print("Minimalna vrijednost: ", min(numbers))
+    print("Maximalna vrijednost: ", max(numbers))
+    numbers.sort()
+    print("Sortirana lista:", numbers)
